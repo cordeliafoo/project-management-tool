85e4d4faae6250fd81b687e9aaa9ead7d8dfd438
Shirong Foo <dee.sr.foo@gmail.com>
2018-09-28 09:18:19 +0800

    Removes code duplication for declaration of initialState

--- project-management-tool/src/redux/reducers/featureReducer.js
+++ project-management-tool/src/redux/reducers/featureReducer.js
@@ -8,51 +8,8 @@ import {
   DELETE_TODO,
 } from '../actions/index.js'
 
-const initialState = {
-  projects: {
-    byId: {
-      project1: {
-        id: 'project1',
-        title: 'project1',
-        features: ['feature1', 'feature2'],
-      },
-      project2: {
-        id: 'project2',
-        title: 'project2',
-      },
-    },
-    allIds: ['project1', 'project2'],
-  },
-  features: {
-    byId: {
-      feature1: {
-        id: 'feature1',
-        title: 'feature1',
-        todos: ['todo1', 'todo2'],
-      },
-      feature2: {
-        id: 'feature2',
-        title: 'feature2',
-      },
-    },
-    allIds: ['feature1', 'feature2'],
-  },
-  todos: {
-    byId: {
-      todo1: {
-        id: 'todo1',
-        title: 'todo1',
-        completed: false,
-      },
-      todo2: {
-        id: 'todo2',
-        title: 'todo2',
-        completed: false,
-      },
-    },
-    allIds: ['todo1', 'todo2'],
-  },
-}
+import initialState from './initialState.js'
+
 function addFeatureEntry(state, action) {
   const { payload } = action
   const { featureId, title } = payload
--- project-management-tool/src/redux/reducers/projectReducer.js
+++ project-management-tool/src/redux/reducers/projectReducer.js
@@ -6,52 +6,7 @@ import {
   ADD_FEATURE,
   DELETE_FEATURE,
 } from '../actions/index.js'
-
-const initialState = {
-  projects: {
-    byId: {
-      project1: {
-        id: 'project1',
-        title: 'project1',
-        features: ['feature1', 'feature2'],
-      },
-      project2: {
-        id: 'project2',
-        title: 'project2',
-      },
-    },
-    allIds: ['project1', 'project2'],
-  },
-  features: {
-    byId: {
-      feature1: {
-        id: 'feature1',
-        title: 'feature1',
-        todos: ['todo1', 'todo2'],
-      },
-      feature2: {
-        id: 'feature2',
-        title: 'feature2',
-      },
-    },
-    allIds: ['feature1', 'feature2'],
-  },
-  todos: {
-    byId: {
-      todo1: {
-        id: 'todo1',
-        title: 'todo1',
-        completed: false,
-      },
-      todo2: {
-        id: 'todo2',
-        title: 'todo2',
-        completed: false,
-      },
-    },
-    allIds: ['todo1', 'todo2'],
-  },
-}
+import initialState from './initialState.js'
 
 function addProjectEntry(state, action) {
   const { payload } = action
